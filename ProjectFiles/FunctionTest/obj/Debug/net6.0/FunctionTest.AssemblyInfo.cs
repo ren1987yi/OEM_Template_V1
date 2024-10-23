2cd4f8b995aacb588c8546f99b0939bac348ccd9
毅 任 <ren1987yi@163.com>
2024-10-23 12:05:05 +0800

    update

--- ProjectFiles/FunctionTest/obj/Debug/net6.0/FunctionTest.AssemblyInfo.cs
+++ ProjectFiles/FunctionTest/obj/Debug/net6.0/FunctionTest.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FunctionTest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+012a8b5edd0b83a0c5d9a3748273ec0460872e18")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+12abf013d62ed5678140cd852ac23f1335a3b957")]
 [assembly: System.Reflection.AssemblyProductAttribute("FunctionTest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FunctionTest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
